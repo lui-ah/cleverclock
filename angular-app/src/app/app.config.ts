f387397deb1a030cd0d9281066b896bbe3fce60e
Luis <luisahrens310@gmail.com>
2024-05-05 22:24:27 +0200

    added angularfire

--- angular-app/src/app/app.config.ts
+++ angular-app/src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { firebaseConfig } from "../environment/environment.prod";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(provideFirebaseApp(() => 
+      initializeApp(firebaseConfig))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFunctions(() => getFunctions()))]
 };
