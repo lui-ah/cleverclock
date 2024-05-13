68d6fa51052fa44dbeea2ddb32ad6811f3beeed9
Luis <luisahrens310@gmail.com>
2024-05-13 09:06:58 +0200

    chore: Update maximumWarning and maximumError values for initial budget in angular.json
    feat: Improve layout on most screens and add minor style adjustments
    
    chore: Remove boilerplate code

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -8,19 +8,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  constructor() {
-    try {
-      // (screen.orientation as any).lock("portrait");
-    } catch (error) { }
-
-    if (false) { // TODO: Make this display if the user is not using a phone in portrait mode
-      alert(""
-        + "Die APP wurde im proof of Concept nur für Handies ausgelegt. \n"
-        + "Ich hab mir keine Mühe gemacht, ein vernünftiges Layout für breite Bildschirme zu erstellen. \n"
-        + "Bitte Benutze die dev tools um eine Handy auflösung zu simulieren, " 
-        + "verkleinere das Fenster deines Browsers oder benutze ein Handy!"
-      );
-    }
-  }
-}
+export class AppComponent {}
